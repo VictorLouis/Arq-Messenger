04cef61f38be54427bd1a54f07106994ff785f70
cjmedinav <cajameve980@gmail.com>
2017-03-06 00:29:49 -0500

    final

--- Arq-Messenger/src/java/DataAccess/DAO/EventDAO.java
+++ Arq-Messenger/src/java/DataAccess/DAO/EventDAO.java
@@ -5,10 +5,44 @@
  */
 package DataAccess.DAO;
 
+import DataAccess.Entity.Event;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
 /**
  *
- * @author felipe
+ * @author arqsoft2017i
  */
 public class EventDAO {
-    
+
+    public EntityManagerFactory emf1 = Persistence.createEntityManagerFactory("MessengerUNPU");
+    public Event persist(Event event) {
+        EntityManager em = emf1.createEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.persist(event);
+            em.getTransaction().commit();
+        } catch(Exception e) {
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+        return event;
+    }
+    public Event searchEvent(String nombre, String descripcion) {
+           EntityManager em = emf1.createEntityManager();
+           Event event = null;
+           String peticion = "select e from Event e where e.nombre = " + nombre + " AND e.descripcion = '"+descripcion+"'";
+           Query q = em.createQuery(peticion);
+           try {
+               event = (Event) q.getSingleResult();
+           } catch (Exception e){
+           } finally {
+               em.close();
+               return event;
+           }
+}
 }
--- Arq-Messenger/src/java/DataAccess/DAO/UserConversationDAO.java
+++ Arq-Messenger/src/java/DataAccess/DAO/UserConversationDAO.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package DataAccess.DAO;
-import DataAccess.Entity.Message;
 import DataAccess.Entity.UserConversation;
 import java.util.List;
 
