8c10deb911fffcc8aaa71fd2ed877ac1ed5fd392
VictorLouis <vlangulot@unal.edu.co>
2017-03-20 11:25:47 -0500

    Querys Corregidas, casi decente

--- DB/TestData.sql
+++ DB/TestData.sql
@@ -1,33 +1,33 @@
 INSERT INTO messengerDB.conversation (Id, `Date`) 
-	VALUES (1, DEFAULT)
+	VALUES (1, DEFAULT);
 
 INSERT INTO messengerDB.UserConversation (IdUser, IdConversation, NombreConversacion) 
-	VALUES (1, 1, 'Prueba1')
+	VALUES (1, 1, 'Prueba1');
 INSERT INTO messengerDB.UserConversation (IdUser, IdConversation, NombreConversacion) 
-	VALUES (2, 1, 'Prueba1')
+	VALUES (2, 1, 'Prueba1');
 
 
 
 INSERT INTO messengerDB.Message (Id, `Date`, Text, IdConversation, User_Id, Hora) 
-	VALUES (1, DEFAULT, 'Sotcio le gustan las estrellas?', 1, 1, DEFAULT)
+	VALUES (1, DEFAULT, 'Sotcio le gustan las estrellas?', 1, 1, DEFAULT);
 INSERT INTO messengerDB.Message (Id, `Date`, Text, IdConversation, User_Id, Hora) 
-	VALUES (2, DEFAULT, 'No se, pues si creo, por?', 1, 2, DEFAULT)
+	VALUES (2, DEFAULT, 'No se, pues si creo, por?', 1, 2, DEFAULT);
 INSERT INTO messengerDB.Message (Id, `Date`, Text, IdConversation, User_Id, Hora) 
-	VALUES (3, DEFAULT, 'Porque miyoz tien 13 lok', 1, 1, DEFAULT)
+	VALUES (3, DEFAULT, 'Porque miyoz tien 13 lok', 1, 1, DEFAULT);
 
 
 INSERT INTO messengerDB.conversation (Id, `Date`) 
-	VALUES (2, DEFAULT)
+	VALUES (2, DEFAULT);
 
 INSERT INTO messengerDB.UserConversation (IdUser, IdConversation, NombreConversacion) 
-	VALUES (3, 1, 'Prueba2')
+	VALUES (3, 2, 'Prueba2');
 INSERT INTO messengerDB.UserConversation (IdUser, IdConversation, NombreConversacion) 
-	VALUES (2, 1, 'Prueba2')
+	VALUES (2, 2, 'Prueba2');
 
 
 INSERT INTO messengerDB.Message (Id, `Date`, Text, IdConversation, User_Id, Hora) 
-	VALUES (1, DEFAULT, 'Que mas? que hace?', 2, 3, DEFAULT)
+	VALUES (4, DEFAULT, 'Que mas? que hace?', 2, 3, DEFAULT);
 INSERT INTO messengerDB.Message (Id, `Date`, Text, IdConversation, User_Id, Hora) 
-	VALUES (2, DEFAULT, 'Nada aca sufriendo con el proyecto', 2, 2, DEFAULT)
+	VALUES (5, DEFAULT, 'Nada aca sufriendo con el proyecto', 2, 2, DEFAULT);
 INSERT INTO messengerDB.Message (Id, `Date`, Text, IdConversation, User_Id, Hora) 
-	VALUES (3, DEFAULT, 'Uyyy re hardcore, suerte', 2, 3, DEFAULT)
+	VALUES (6, DEFAULT, 'Uyyy re hardcore, suerte', 2, 3, DEFAULT);
